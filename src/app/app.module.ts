a25c5bbd7b1422d55b4f0039ebb4fefef3091b1c
U_Montag <montagu666@gmail.com>
2022-09-08 10:46:10 +0200

    Add service to fetch mock-data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+
 @NgModule({
   declarations: [AppComponent, MenuComponent, DishdetailComponent],
   imports: [
@@ -24,7 +26,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatButtonModule,
     MatCardModule,
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
